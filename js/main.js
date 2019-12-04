268cefafeef0bd71c0804da2442460fb0ce17aea
孙通 <17701253801@163.com>
2019-12-05 00:21:50 +0800

    init code

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+    test();
+});
+
+function test() {
+    let array = ["赵", "钱", "孙"];
+    array.forEach(element => {
+        console.log(element)
+    });
+};
---
+++ shell/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+BOOTNAME=$1
+
+echo $BOOTNAME
