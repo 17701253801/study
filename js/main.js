6f08dc20871e6ddd9154d007698758b4c0a8ecaf
kiwi <17701253801@163.com>
2020-07-14 23:13:15 +0800

    UPDATE: master

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $(function () {
 
 function test() {
     let array = ["赵", "钱", "孙","zhou","zhengwang"];
+    console.log("Hello world!")
     array.forEach(element => {
         console.log(element)
     });
